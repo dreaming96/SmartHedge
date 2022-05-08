616e4ff9c8495182b0c53e0e6ea6df738de2bfab
c w <chrischoat@hotmail.com>
2022-05-08 15:57:00 +0100

    Added functionality to retrieve and store Black-Scholes option prices and greeks.

--- data_handling.py
+++ data_handling.py
@@ -8,12 +8,12 @@ from scipy.stats import norm, poisson, gamma
 class numerical_methods():
     def __init__(self, paths):
         self.S0 = 100
-        self.r = 0.01
+        self.r = 0.1
         self.mu = 0.05
         self.sigma = 0.2
         self.T = 1
-        self.N = 252*10
-        self.I = 1000
+        self.N = 252
+        self.I = 500
         self.paths = paths
 
     def MC(self):
@@ -23,10 +23,12 @@ class numerical_methods():
         for t in range(1, self.N+1):
             rand = np.random.standard_normal(self.I)
             self.paths[t] = self.paths[t - 1] * np.exp((self.r - 0.5 * self.sigma ** 2) * dt + self.sigma * np.sqrt(dt) * rand)
+        self.paths = pd.DataFrame(self.paths)
         self.plot()
         return self.paths
 
     def plot(self):
+        plt.title("Price paths")
         plt.xlabel("Time")
         plt.ylabel("Price")
         plt.plot(self.paths, linewidth=0.5)
@@ -34,33 +36,39 @@ class numerical_methods():
         return None
 
 class BS_pricer():
-    def __init__(self):
+    def __init__(self, S, call_delta, call_gamma, call_vega, call_theta, put_delta, put_gamma, put_vega, put_theta):
         self.S = S
-        self.K = K
-        self.r = r
-        self.T = T
-        self.sigma = sigma
-        self.d1 = (np.log(self.S / self.K) + (self.r + self.sigma ** 2) * T) / self.sigma * np.sqrt(self.T)
-        self.d2 = self.d1 - self.sigma * np.sqrt(self.T)
+        self.K = 100
+        self.r = 0.1
+        self.ttm = 252
+        self.sigma = 0.2
+        self.d1 = (np.log(self.S / self.K) + (self.r + self.sigma ** 2) * self.ttm) / self.sigma * np.sqrt(self.ttm)
+        self.d2 = self.d1 - self.sigma * np.sqrt(self.ttm)
+        self.call_delta = call_delta
+        self.call_gamma = call_gamma
+        self.call_vega = call_vega
+        self.call_theta = call_theta
+        self.put_delta = put_delta
+        self.put_gamma = put_gamma
+        self.put_vega = put_vega
+        self.put_theta = put_theta
 
     def call_price(self):
-        price = self.S * norm.cdf(self.d1) - self.K * np.exp(-self.r * self.T) * norm.cdf(self.d2)
+        price = self.S * norm.cdf(self.d1) - self.K * np.exp(-self.r * self.ttm) * norm.cdf(self.d2)
         return price
 
     def put_price(self):
-        price = self.K * np.exp(-self.r * self.T) - self.S + (self.S * norm.cdf(self.d1) - self.K * np.exp(-self.r * self.T) * norm.cdf(self.d2))
+        price = self.K * np.exp(-self.r * self.ttm) - self.S + (self.S * norm.cdf(self.d1) - self.K * np.exp(-self.r * self.ttm) * norm.cdf(self.d2))
         return price
 
-    def greeks(self, price, type):
-        call_delta = norm.cdf(self.d1)
-        call_gamma = norm.pdf(self.d1) / (self.S * self.sigma * np.sqrt(self.T))
-        call_vega = self.S * norm.pdf(self.d1) * np.sqrt(self.T)
-        call_theta = - ((self.S * norm.pdf(self.d1) * self.sigma) / 2 * np.sqrt(self.T)) - self.r * self.K * np.exp(-self.r * self.T) * norm.cdf(self.d2)
-        put_delta = norm.cdf(self.d1) - 1
-        put_gamma = norm.pdf(self.d1) / (self.S * self.sigma * np.sqrt(self.T))
-        put_vega = self.S * norm.pdf(self.d1) * np.sqrt(self.T)
-        put_theta = - ((self.S * norm.pdf(self.d1) * self.sigma) / 2 * np.sqrt(self.T)) + self.r * self.K * np.exp(-self.r * self.T) * norm.cdf(-self.d1)
+    def greeks(self, type):
         if type.upper() == "CALL":
-            return call_delta, call_gamma, call_vega, call_theta
+            self.call_delta.append(norm.cdf(self.d1))
+            self.call_gamma.append(norm.pdf(self.d1) / (self.S * self.sigma * np.sqrt(self.ttm)))
+            self.call_vega.append(self.S * norm.pdf(self.d1) * np.sqrt(self.ttm))
+            self.call_theta.append(- ((self.S * norm.pdf(self.d1) * self.sigma) / 2 * np.sqrt(self.ttm)) - self.r * self.K * np.exp(-self.r * self.ttm) * norm.cdf(self.d2))
         else:
-            return put_delta, put_gamma, put_vega, put_theta
+            self.put_delta.append(norm.cdf(self.d1) - 1)
+            self.put_gamma.append(norm.pdf(self.d1) / (self.S * self.sigma * np.sqrt(self.ttm)))
+            self.put_vega.append(self.S * norm.pdf(self.d1) * np.sqrt(self.ttm))
+            self.put_theta.append(- ((self.S * norm.pdf(self.d1) * self.sigma) / 2 * np.sqrt(self.ttm)) + self.r * self.K * np.exp(-self.r * self.ttm) * norm.cdf(-self.d1))
--- wrapper.py
+++ wrapper.py
@@ -1,10 +1,39 @@
 from data_handling import numerical_methods, BS_pricer
 
 if __name__ == "__main__":
+    type = ["call", "put"]
     #loop for train and test (2x MC)
-    instance = numerical_methods([])
-    numerical_methods.MC(instance)
-    BS_pricer.call_price()
-    BS_pricer.put_price()
-    BS_pricer.greeks()
+    mc_instance = numerical_methods([])
+    numerical_methods.MC(mc_instance)
+    simulated_prices = mc_instance.paths
+    call_prices = simulated_prices * 0
+    call_delta = call_prices.copy()
+    call_gamma = call_prices.copy()
+    call_vega = call_prices.copy()
+    call_theta = call_prices.copy()
+    put_prices = simulated_prices * 0
+    put_delta = put_prices.copy()
+    put_gamma = put_prices.copy()
+    put_vega = put_prices.copy()
+    put_theta = put_prices.copy()
+    for i, row in simulated_prices.iterrows():
+        for idx, j in enumerate(row):
+            bs_instance = BS_pricer(S=j, call_delta=[], call_gamma=[], call_vega=[], call_theta=[], put_delta=[], put_gamma=[], put_vega=[], put_theta=[])
+            for k in type:
+                if k.upper() == "CALL":
+                    call_price = BS_pricer.call_price(bs_instance)
+                    call_prices.iloc[i, idx] = call_price
+                    BS_pricer.greeks(bs_instance, k)
+                    call_delta.iloc[i, idx] = bs_instance.call_delta[0]
+                    call_gamma.iloc[i, idx] = bs_instance.call_gamma[0]
+                    call_vega.iloc[i, idx] = bs_instance.call_vega[0]
+                    call_theta.iloc[i, idx] = bs_instance.call_theta[0]
+                else:
+                    put_price = BS_pricer.put_price(bs_instance)
+                    put_prices.iloc[i, idx] = put_price
+                    BS_pricer.greeks(bs_instance, k)
+                    put_delta.iloc[i, idx] = bs_instance.put_delta[0]
+                    put_gamma.iloc[i, idx] = bs_instance.put_gamma[0]
+                    put_vega.iloc[i, idx] = bs_instance.put_vega[0]
+                    put_theta.iloc[i, idx] = bs_instance.put_theta[0]
     print(123)
